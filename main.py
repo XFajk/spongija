fc50384d49326387d80b1cd99985f249f9463421
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2023-10-22 14:54:45 +0200

    added some basic stuff so everything works out of the box

--- main.py
+++ main.py
@@ -13,12 +13,32 @@ def main():
 
     clock = pygame.time.Clock()
 
+    last_frame_time = time.perf_counter()
+
     done = False
     while not done:
+
+        current_frame_time = time.perf_counter()
+        dt = (current_frame_time - last_frame_time) * 60
+
+        keys = pygame.key.get_pressed()
+
+        mouse_input = pygame.mouse.get_pressed()
+        mouse_pos = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+
+        display.fill((255, 255, 255))
+
+        surf = pygame.transform.scale(display, WS)
+        window.blit(surf, [0, 0])
+        pygame.display.update()
+
+        pygame.display.set_caption(f"<game name> FPS: {round(clock.get_fps(), 2)}")
+
+        clock.tick(120)
 
     pygame.quit()
 
