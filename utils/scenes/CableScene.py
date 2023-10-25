1c71eb4e4a18bbfc911a8e3c0b7ce0603dfe4eda
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2023-10-25 23:44:40 +0200

    added some stuff to the cable scene the screws are done and the plate is gradable and replaced raketanedokoncena.png with a new version

--- utils/scenes/CableScene.py
+++ utils/scenes/CableScene.py
@@ -26,27 +26,47 @@ class CableScene:
             [Interactable((display.get_width() - 15 * 2 - 5, 15), (8, 8)), False, 0.0]
         ]
         self.screw_image: pygame.Surface = pygame.image.load("Assets/sprites/screw.png").convert_alpha()
-        self.upper_plate: Interactable = Interactable((15, 10),
-                                                      (display.get_width() - 15 * 2, display.get_height() - 20 * 2))
+
+        self.upper_plate: Interactable = Interactable(
+            (15, 10),
+            (display.get_width() - 15 * 2, display.get_height() - 20 * 2)
+        )
+        self.electrical_box: pygame.Surface = pygame.image.load("Assets/sprites/electrical_box.png").convert_alpha()
+        ic(self.upper_plate.rect)
+        self.mouse_plate_delta: tuple = (0, 0)
 
     def play(self, dt: float, tool_bar: ToolBar, display: pygame.Surface, mouse_pos: tuple,
              interaction_starter: bool) -> None:
 
+        self.upper_plate.update(mouse_pos, interaction_starter)
+
         display.fill((90, 90, 90))
 
+        # electrical box drawing
+        display.blit(self.electrical_box, (15, 10))
+
+        # upper plate logic and drawing
+        if not len(self.screws) and self.upper_plate.is_held and tool_bar.current_tool.name == "grab":
+            if self.upper_plate.is_clicked:
+                self.mouse_plate_delta = mouse_pos[0] - self.upper_plate.rect.x, mouse_pos[1] - self.upper_plate.rect.y
+
+            self.upper_plate.rect.x = mouse_pos[0] - self.mouse_plate_delta[0]
+            self.upper_plate.rect.y = mouse_pos[1] - self.mouse_plate_delta[1]
+
         pygame.draw.rect(display, (80, 80, 90), self.upper_plate.rect)
+
+        # screws logic and drawing
         for i, screw in enumerate(self.screws):
             screw[0].update(mouse_pos, interaction_starter)
             if tool_bar.current_tool.name == "screw_driver" and screw[0].is_held:
                 screw[2] += 5 * dt
                 if screw[2] > 360:
                     self.screws.pop(i)
-            ic(screw[2])
             rotated_screw = pygame.transform.rotate(self.screw_image, screw[2])
             display.blit(
                 rotated_screw,
                 (
-                    screw[0].rect.x - rotated_screw.get_width() / 2 + self.screw_image.get_width()/2,
-                    screw[0].rect.y - rotated_screw.get_height() / 2 + self.screw_image.get_height()/2
+                    screw[0].rect.x - rotated_screw.get_width() / 2 + self.screw_image.get_width() / 2,
+                    screw[0].rect.y - rotated_screw.get_height() / 2 + self.screw_image.get_height() / 2
                 )
             )
